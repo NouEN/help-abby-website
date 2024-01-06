5464dda7e508762de1281d77c867fa73ef7884aa
NouEN <noviaaen@gmail.com>
2024-01-06 23:47:41 +0700

    add nutri value and pagination

--- src/components/NutritionalValuePage.tsx
+++ src/components/NutritionalValuePage.tsx
@@ -3,6 +3,8 @@ import AdminNavbar from "./AdminNavbar";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import "../style.css";
+import ReactPaginate from "react-paginate";
 
 interface Food {
   foodId: number;
@@ -16,6 +18,8 @@ interface Food {
 
 function NutritionalValuePage() {
   const [foodList, setFoodList] = useState([]);
+  const [itemOffset, setItemOffset] = useState(0);
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -27,19 +31,62 @@ function NutritionalValuePage() {
     console.log(foodList);
     console.log(response.data);
   };
+
+  const endOffset = itemOffset + 5;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const foodListSliced = foodList.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(foodList.length / 5);
+  const handleClickPage = (event) => {
+    const newOffset = (event.selected * 10) % foodList.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
   return (
-    <div className="main-section">
-      <h1>All Books</h1>
-      <div className="list">
-        {foodList.map((food: Food, index) => (
-          <ul>
-            <li>Food Name: {food.foodName}</li>
-            <li>Calories: {food.calories}</li>
-            <li>Carbs: {food.carbs}</li>
-            <li>Protein: {food.protein}</li>
-            <li>Fats: {food.fat}</li>
-          </ul>
-        ))}
+    <div className="page">
+      <div className="section">
+        <div className="container">
+          <div className="subtitle">
+            <Link to="/food" className="subtitle">
+              ⬅️Back to Food
+            </Link>
+          </div>
+          <h1 className="title-page">Nutritional Value of Foods</h1>
+        </div>
+      </div>
+
+      <div className="section">
+        <div className="container">
+          <div className="list">
+            {foodListSliced.map((food: Food, index) => (
+              <div className="row">
+                <div className="article">
+                  <div className="frame">
+                    <div className="icon">⭐</div>
+                    <div className="title-3">{food.foodName}</div>
+                    <div className="subtitle"> {food.calories} kcal</div>
+                    <div className="div">Carbs: {food.carbs}g </div>
+                    <div className="div">Protein: {food.protein}g </div>
+                    <div className="div">Fats: {food.fat}g </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div>
+            <ReactPaginate
+              breakLabel="..."
+              nextLabel="Next"
+              onPageChange={handleClickPage}
+              pageCount={pageCount}
+              previousLabel="Prev"
+              renderOnZeroPageCount={null}
+              className="paginate"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
