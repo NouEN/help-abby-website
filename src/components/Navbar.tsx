7cb46dddc36ef9766f0f7620f8256f9ff3c9f67c
NouEN <noviaaen@gmail.com>
2024-01-09 22:14:06 +0700

    fix navbar width

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,40 +5,34 @@ import "../style.css";
 
 function Navbar() {
   return (
-    <nav>
-      <div>
-        <div>
-          <div className="top-bar">
-            <div className="rectangle" />
-            <div className="title">HelpAbby</div>
-            <ul className="navbar-nav">
-              <div className="navbar">
-                <li className="nav-item">
-                  <Link to="/" className="nav-link" aria-current="page">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item active">
-                  <Link to="/food" className="nav-link">
-                    Food
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/training" className="nav-link">
-                    Training
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/myths" className="nav-link">
-                    Myths
-                  </Link>
-                </li>
-              </div>
-            </ul>
-          </div>
+    <div className="navbar-top">
+      <div className="rectangle" />
+      <div className="title">HelpAbby</div>
+      <ul className="navbar-nav">
+        <div className="navbar">
+          <li className="nav-item">
+            <Link to="/" className="nav-link" aria-current="page">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item active">
+            <Link to="/food" className="nav-link">
+              Food
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/training" className="nav-link">
+              Training
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/myths" className="nav-link">
+              Myths
+            </Link>
+          </li>
         </div>
-      </div>
-    </nav>
+      </ul>
+    </div>
   );
 }
 
